89926ce1acc8fb23870d0d85d0d50a69f135e8c8
KtorZ <matthias.benkort@gmail.com>
2018-05-09 18:21:38 +0200

    [CSL-2492] Run the API QuickCheck integration tests through HTTPs

--- wallet-new/integration/QuickCheckSpecs.hs
+++ wallet-new/integration/QuickCheckSpecs.hs
@@ -1,9 +1,9 @@
-{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE NumDecimals #-}
+{-# LANGUAGE PolyKinds   #-}
 
-module QuickCheckSpecs (spec) where
+module QuickCheckSpecs (mkSpec) where
 
-import Universum
+import           Universum
 
 import           Network.HTTP.Client hiding (Proxy)
 import           Network.HTTP.Types
@@ -23,19 +23,26 @@ import           Cardano.Wallet.API.V1.Parameters (WalletRequestParams, WithWall
 -- browsers can be hacked: https://haacked.com/archive/2009/06/25/json-hijacking.aspx/
 -- The general consensus, after discussing this with the team, is that we can be moderately safe.
 -- stack test cardano-sl-wallet-new --fast --test-arguments '-m "Servant API Properties"'
-spec :: Spec
-spec = do
+mkSpec :: Manager -> Spec
+mkSpec mgr = do
+    -- NOTE
+    -- We need this because there's currently no way to specify a custom manager
+    -- for the request predicate. The BaseUrl is still needed for the call to
+    -- serverSatisfies but it won't probably reflect the right target for all
+    -- request predicate for in these cases, the url and port will come from the
+    -- Manager itself.
+    -- Have a look at where the Manager is defined (likely Main.hs)
     let burl = BaseUrl
-            { baseUrlScheme = Http
+            { baseUrlScheme = Https
             , baseUrlHost = "localhost"
             , baseUrlPort = 8090
             , baseUrlPath = ""
             }
     describe "Servant API Properties" $ do
         it "V0 API follows best practices & is RESTful abiding" $ do
-            serverSatisfies (Proxy @V0.API) burl stdArgs predicates
+            serverSatisfies (Proxy @V0.API) burl stdArgs (predicates mgr)
         it "V1 API follows best practices & is RESTful abiding" $ do
-            serverSatisfies (Proxy @V1.API) burl stdArgs predicates
+            serverSatisfies (Proxy @V1.API) burl stdArgs (predicates mgr)
 --
 -- Instances to allow use of `servant-quickcheck`.
 --
@@ -67,8 +74,8 @@ instance HasGenRequest (sub :: *) => HasGenRequest (WalletRequestParams :> sub) 
 --
 
 -- | Checks that every DELETE request should return a 204 NoContent.
-deleteReqShouldReturn204 :: RequestPredicate
-deleteReqShouldReturn204 = RequestPredicate $ \req mgr ->
+deleteReqShouldReturn204 :: Manager -> RequestPredicate
+deleteReqShouldReturn204 mgr = RequestPredicate $ \req _ -> do
     if (method req == methodDelete)
         then do
             resp <- httpLbs req mgr
@@ -81,8 +88,8 @@ deleteReqShouldReturn204 = RequestPredicate $ \req mgr ->
 
 -- | Checks that every PUT request is idempotent. Calling an endpoint with a PUT
 -- twice should return the same result.
-putIdempotency :: RequestPredicate
-putIdempotency = RequestPredicate $ \req mgr ->
+putIdempotency :: Manager -> RequestPredicate
+putIdempotency mgr = RequestPredicate $ \req _ ->
      if (method req == methodPut)
          then do
              resp1 <- httpLbs req mgr
@@ -97,8 +104,8 @@ putIdempotency = RequestPredicate $ \req mgr ->
 
 -- | Checks that every request which is not a 204 No Content
 -- does not have an empty body, but it always returns something.
-noEmptyBody :: RequestPredicate
-noEmptyBody = RequestPredicate $ \req mgr -> do
+noEmptyBody :: Manager -> RequestPredicate
+noEmptyBody mgr = RequestPredicate $ \req _ -> do
     resp <- httpLbs req mgr
     let body   = responseBody resp
     let status = responseStatus resp
@@ -107,9 +114,9 @@ noEmptyBody = RequestPredicate $ \req mgr -> do
     return [resp]
 
 -- | All the predicates we want to enforce in our API.
-predicates :: Predicates
-predicates = not500
-         <%> deleteReqShouldReturn204
-         <%> putIdempotency
-         <%> noEmptyBody
+predicates :: Manager -> Predicates
+predicates mgr = not500
+         <%> deleteReqShouldReturn204 mgr
+         <%> putIdempotency mgr
+         <%> noEmptyBody mgr
          <%> mempty
